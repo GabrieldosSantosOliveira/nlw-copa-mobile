73be845acc973b5ac721a2da04cf2495159a7af1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 10:56:22 -0400

    feat: Add screen SingIn

---
+++ src/screens/SingIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base"
+export function SingIn(){
+return (
+  <Center   flex={1} bg="gray.900">
+      
+         <Text  color="white" fontSize={24}>Open up App.js to start working on your app!</Text> 
+      
+      
+    </Center>
+)
+}

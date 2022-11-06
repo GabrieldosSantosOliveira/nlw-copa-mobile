49cad576cf13f1b2c1f53400d566685465c26f42
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 12:17:38 -0400

    feat: Add component Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,41 @@
+import {
+  Button as ButtonNativeBase,
+  Text,
+  IButtonProps
+} from 'native-base';
+import React from 'react';
+interface Props extends IButtonProps {
+  title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
+}
+export const Button: React.FC<Props> = ({
+  title,
+  type = 'PRIMARY',
+  ...rest
+}) => {
+  return (
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+      }}
+      _loading={{
+        _spinner: { color: 'black' }
+      }}
+      {...rest}
+    >
+      <Text
+        fontSize="sm"
+        fontFamily="heading"
+        color={type === 'SECONDARY' ? 'white' : 'black'}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  );
+};

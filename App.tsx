56e1cfe12966888ff2bae8a060c6bc2beee6610f
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 12:16:42 -0400

    feat: Add AuthContextProvider

--- App.tsx
+++ App.tsx
@@ -1,28 +1,32 @@
-import {NativeBaseProvider, StatusBar} from 'native-base'
-import {THEME} from './src/styles/theme'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_500Medium
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
 import { Loading } from './src/components/Loading';
-import {useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium} from '@expo-google-fonts/roboto'
+import { AuthContextProvider } from './src/context/AuthContext';
 import { SingIn } from './src/screens/SingIn';
+import { THEME } from './src/styles/theme';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     Roboto_500Medium
-  })
- 
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-      barStyle='light-content'
-      backgroundColor='transparent'
-      translucent
-      />
-      {
-        fontsLoaded ? <SingIn/>: <Loading/> 
-      }
-      
-     
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SingIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
-

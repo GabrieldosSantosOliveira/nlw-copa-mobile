bb2f3c36a01fd1255e8a7755d27cc867ec43ce05
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 12:20:48 -0400

    feat: Added SingIn page components

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,11 +1,33 @@
-import { Center, Text } from "native-base"
-export function SingIn(){
-return (
-  <Center   flex={1} bg="gray.900">
-      
-         <Text  color="white" fontSize={24}>Open up App.js to start working on your app!</Text> 
-      
-      
+import { Fontisto } from '@expo/vector-icons';
+import { Center, Icon, Text } from 'native-base';
+
+import Logo from './../assets/logo.svg';
+import { Button } from './../components/Button';
+import { useAuth } from './../hooks/useAuth';
+export function SingIn() {
+  const { singIn, user } = useAuth();
+  console.log('Dados do usuário: ' + JSON.stringify(user));
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="Entrar com o google"
+        mt={12}
+        leftIcon={
+          <Icon
+            as={Fontisto}
+            name="google"
+            color="white"
+            size="md"
+          />
+        }
+        onPress={singIn}
+        type="SECONDARY"
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu
+        e-mail para criação de sua conta.
+      </Text>
     </Center>
-)
+  );
 }

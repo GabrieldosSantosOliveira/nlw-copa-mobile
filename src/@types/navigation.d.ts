f937edffe63c1ecf0edd1777950b988520e5ed67
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-06 21:28:26 -0400

    feat: Add type navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,13 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList
+      extends Record<string, object | undefined> {
+      new: undefined;
+      pools: undefined;
+      find: undefined;
+      details: {
+        id: string;
+      };
+    }
+  }
+}

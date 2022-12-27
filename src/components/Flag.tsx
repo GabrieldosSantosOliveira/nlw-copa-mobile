fad8f7a66aeb6a48cb14f652c705fcf7e64a22b0
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-27 09:14:55 -0400

    feat: Add component Flag

--- src/components/Flag.tsx
+++ src/components/Flag.tsx
@@ -2,12 +2,6 @@ import { Image, IImageProps } from 'native-base';
 
 export function Flag({ ...rest }: IImageProps) {
   return (
-    <Image
-      {...rest}
-      alt="Bandeira"
-      w={8}
-      h={6}
-      mx={3}
-    />
+    <Image {...rest} alt="Bandeira" w={8} h={6} mx={3} />
   );
 }

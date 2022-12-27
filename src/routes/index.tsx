bc7b2275afd0f76c7c3e42cf4d28860f24cd6039
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-27 09:42:34 -0400

    feat: Added route authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { Box } from 'native-base';
 
+import { SingIn } from '../screens/SingIn';
+import { useAuth } from './../hooks/useAuth';
 import { AppRoutes } from './app.routes';
 export const Routes = () => {
+  const { user } = useAuth();
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bgColor="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SingIn />}
+      </NavigationContainer>
+    </Box>
   );
 };

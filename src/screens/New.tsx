f75ddf00f21e44d10e5db9cc32bc2b81b5a81b11
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-27 09:48:47 -0400

    feat:  Add function handlePoolCreate for create pool

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,10 +1,47 @@
-import { Heading, Text, VStack } from 'native-base';
+import {
+  Heading,
+  Text,
+  VStack,
+  useToast
+} from 'native-base';
+import { useState } from 'react';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
+import { api } from '../services/api';
 import Logo from './../assets/logo.svg';
 export const New = () => {
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const handlePoolCreate = async () => {
+    if (!title) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    }
+    try {
+      setIsLoading(true);
+      await api.post('/pools', { title });
+      toast.show({
+        title: 'Bolão criado com sucesso',
+        placement: 'top',
+        bgColor: 'green.300'
+      });
+      setTitle('');
+    } catch (error) {
+      toast.show({
+        title: 'Não foi possivel criar o bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -22,9 +59,15 @@ export const New = () => {
         </Heading>
         <Input
           mb={2}
-          placeholder="Qual nome do seu Bolão?"
+          placeholder="Qual nome do seu bolão?"
+          onChangeText={setTitle}
+          value={title}
         />
-        <Button title="CRIAR O MEU BOLÃO" />
+        <Button
+          title="CRIAR O MEU BOLÃO"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
+        />
         <Text
           color="gray.200"
           fontSize="sm"
